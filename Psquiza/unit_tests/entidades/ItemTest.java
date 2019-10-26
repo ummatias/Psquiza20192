efc5f9d6f97c5613d0ec3216869e8010307154b7
joseigorgomes <jose.gomes@ccc.ufcg.edu.br>
2019-10-26 20:26:58 -0300

    Testes de Unidade do CdU 4 finalizados

---
+++ Psquiza/unit_tests/entidades/ItemTest.java
@@ -0,0 +1,27 @@
+package entidades;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ItemTest {
+
+	private Item item;
+
+	@BeforeEach
+	void inicializaItem() {
+		this.item = new Item("Preparar insumos do laboratório de biologia.");
+	}
+	
+	@Test
+	void testToString() {
+		assertEquals("PENDENTE - Preparar insumos do laboratório de biologia.", item.toString());
+	}
+	
+	@Test
+	void testGetStatus() {
+		assertFalse(item.getStatus());
+	}
+
+}
