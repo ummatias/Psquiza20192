dac9b54944437d13153cc7b47f2d627bb67e57b8
natalia-sa <nataliasalvino86@gmail.com>
2019-10-29 09:57:37 -0300

    documentei a classe pesquisa

--- Psquiza/src/entidades/Pesquisa.java
+++ Psquiza/src/entidades/Pesquisa.java
@@ -1,13 +1,35 @@
 package entidades;
 
 import validadores.ValidadorEntradas;
-
+/**
+ * Classe que representa a pesquisa, contendo uma descricao, campo de interesse
+ * um codigo unico e um status
+ * @author Natalia salvino 
+ *
+ */
 public class Pesquisa {
+/**
+ * Descricao da pesquisa
+ */
 	private String descricao;
+/**
+ * Campo de interesse da pesquisa
+ */
 	private String campoDeInteresse;
+/**
+ * O codigo que identifica a pesquisa
+ */
 	private String codigo;
+/**
+ * O status da pesquisa, que pode ser ativo ou inativo
+ */
 	private boolean status;
-	
+/**
+ * Contrutor da pesquisa
+ * @param codigo o codigo da pesquisa
+ * @param descricao a descricao da pesquisa
+ * @param campoDeInteresse
+ */
 	public Pesquisa(String codigo,String descricao, String campoDeInteresse){
 		ValidadorEntradas.validarString(descricao,"Descricao nao pode ser nula ou vazia.");
 		ValidadorEntradas.validarString(campoDeInteresse,"Formato do campo de interesse invalido.");
@@ -36,6 +58,9 @@ public class Pesquisa {
 	}
 	public void desativaPesquisa() {
 		this.status = false;
+	}
+	public boolean getStatus() {
+		return this.status;
 	}
 	
 
