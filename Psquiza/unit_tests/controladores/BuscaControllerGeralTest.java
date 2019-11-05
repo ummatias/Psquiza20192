a518cf48b897171fa17a27259ffcad847dd0ef66
Lenovo <nataliasalvino86@gmail.com>
2019-11-04 22:56:43 -0300

    adicionando testes no controllerGeralTest

--- Psquiza/unit_tests/controladores/BuscaControllerGeralTest.java
+++ Psquiza/unit_tests/controladores/BuscaControllerGeralTest.java
@@ -1,9 +1,12 @@
 package controladores;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import generaliza.ControllerGeral;
 
 public class BuscaControllerGeralTest {
 	private ControllerGeral controller;
@@ -11,7 +14,7 @@ public class BuscaControllerGeralTest {
 	@BeforeEach
 	void setUp() {
 		this.controller = new ControllerGeral();
-		controller.cadastraPesquisa("Adesão crescente ao movimento Calculo 2020.1", 5);
+		controller.cadastraPesquisa("Adesão crescente ao movimento Calculo 2020.1", "calculo, 2010");
 		controller.cadastraPesquisador("Maria","externo", "grande interesse em filmes de comedia e computação", "maria@gmail.com", "https://foto.com");
 		controller.cadastraProblema("Falta de bom senso dos estudantes", 3);
 		controller.cadastraObjetivo("Especifico", "Estudar história e ver menos filme de ação", 5, 5);
@@ -39,5 +42,21 @@ public class BuscaControllerGeralTest {
 		assertThrows( NullPointerException.class, () -> {
 			controller. contaResultadosBusca(null);
 		});
+	}
+	@Test
+	void testAssociaProblemaComSucesso() {
+		assertEquals(true,controller.associaProblema("CAL1", "P1"));
+	}
+	@Test
+	void testDesassociaProblemaComSucesso() {
+		assertEquals(true,controller.desassociaProblema("CAL1", "P1"));
+	}
+	@Test
+	void testAssociaObjetivoComSucesso() {
+		assertEquals(true, controller.associaObjetivo("CAL1", "O1"));
+	}
+	@Test
+	void testDesassociaObjetivoComSucesso() {
+		assertEquals(true,controller.desassociaObjetivo("CAL1", "O1"));
 	}
 }
