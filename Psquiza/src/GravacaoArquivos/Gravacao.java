2a78d0d4d98173e3acf714479d6cd458866b7119
natalia-sa <natalia.andre@ccc.ufcg.edu.br>
2019-11-15 20:36:38 -0300

    inicando a cdu 12

---
+++ Psquiza/src/GravacaoArquivos/Gravacao.java
@@ -0,0 +1,53 @@
+package GravacaoArquivos;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Gravacao {
+	public static void salvar(Object objeto, String caminho) {
+
+		try {
+
+			FileOutputStream saveFile = new FileOutputStream(caminho);
+
+			ObjectOutputStream stream = new ObjectOutputStream(saveFile);
+
+			stream.writeObject(objeto);
+
+			stream.close();
+
+		} catch (Exception exc) {
+
+			exc.printStackTrace();
+
+		}
+
+	}
+
+	public static Object restaurar(String caminho) {
+
+		Object objeto = null;
+
+		try {
+
+			FileInputStream restFile = new FileInputStream(caminho);
+
+			ObjectInputStream stream = new ObjectInputStream(restFile);
+
+
+			objeto = stream.readObject();
+
+			stream.close();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		return objeto;
+
+	}
+}
